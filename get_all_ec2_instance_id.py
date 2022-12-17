56f71b176b24510d51d2b4d53303f2c4a159546b
nasreddinelaouar <laouarn@hotmail.com>
2022-12-17 16:17:50 +0000

    commit on main

---
+++ get_all_ec2_instance_id.py
@@ -0,0 +1,10 @@
+#Get all EC2 instance IDs using boto3 from python
+import boto3
+
+ec2_client = boto3.client("ec2")
+x = ec2_client.describe_instances()
+data = x["Reservations"][0]
+data_instance = data["Instances"]
+for i in range (len(data_instance)):
+    print(f"instance id is {data_instance[i]['InstanceId']}")
+    
